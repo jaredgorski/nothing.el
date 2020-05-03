5a6779ad97ba4b443a89a7c6d041f3b4cc1b1a2e
jaredgorski <jared.gorski@liferay.cloud>
2020-05-03 01:44:08 -0700

    Update

--- nothing-theme.el
+++ nothing-theme.el
@@ -1,37 +1,33 @@
 ;;; nothing-theme.el --- Monochrome emacs
 
-
-;; Copyright (C) 2020 Jared Gorski
-
-;; Author: Jared Gorski <jaredgorski6@gmail.com>
-;; Version: 1.0
+;; Copyright (C) 2020, Jared Gorski, <jaredgorski6@gmail.com>
+;;
+;; Version: 1.0.0
+;; Keywords: colorscheme theme monochrome minimal
+;; Author: Jared Gorski, <jaredgorski6@gmail.com>
+;; URL: http://github.com/jaredgorski/nothing.el
 ;; Package-Requires: ((emacs "24"))
-;; Keywords: colorscheme, theme, monochrome, minimal
-;; URL: https://github.com/jaredgorski/nothing.el
 
-
-;; This program is free software: you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
+;; the Free Software Foundation; either version 3 of the License, or
 ;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
-
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 ;; This file is not part of Emacs.
-
 
 ;;; Commentary:
 
 ;; This theme provides a monochrome and rather default-style emacs
 ;; experience. This helps some people focus on content over form and
 ;; potentially minimizes effects of context-switching.
-
 
 ;;; Code:
 
